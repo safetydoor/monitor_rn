383b7efe0b3dee456a6261b060ae0126bb3ea9dc
laps <liaopanpan610@pingan.com.cn>
2016-12-10 17:46:16 +0800

    add yoosee

--- rn/index.ios.js
+++ rn/index.ios.js
@@ -11,74 +11,16 @@ import {
     Text,
     View
 } from 'react-native';
-const SideMenu = require('react-native-side-menu');
-
-
-class ContentView extends React.Component {
-  render() {
-    return (
-        <View style={styles.container}>
-<Text style={styles.welcome}>
-Welcome to React Native!
-</Text>
-<Text style={styles.instructions}>
-To get started, edit index.ios.js
-</Text>
-<Text style={styles.instructions}>
-Press Cmd+R to reload,{'\n'}
-Cmd+Control+Z for dev menu
-</Text>
-</View>
-);
-}
-}
-
-class Menu extends React.Component {
-  render() {
-
-    return (
-        < View >
-        < Text > sdfsdfsd < / Text >
-        < / View >
-  )
-    ;
-  }
-}
-
 
 class monitor extends Component {
-  render() {
-    const menu =
-  <
-    Menu
-    navigator = {navigator} / >;
 
-    return (
-        < SideMenu
-    menu = {menu} >
-        < ContentView / >
-        < / SideMenu >
-  )
-    ;
-  }
+    render() {
+        return (
+            <View>
+                <Text>asdgsdgdsfg</Text>
+            </View>
+        )
+    }
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('monitor', () => monitor);
